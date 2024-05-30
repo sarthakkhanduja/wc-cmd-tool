25d5b9ac91a6f92ff53d1a16cd344230bd58c790
sarthakkhanduja <sarthakkhanduja98@gmail.com>
2024-05-30 14:26:39 -0400

    Completed step 1 of the coding challenge

--- main.py
+++ main.py
@@ -1,4 +1,35 @@
-import argparse
+import sys
+import os
 
-parser = argparse.ArgumentParser()
-parser.parse_args()
+
+def countBytes(fileName):
+    f = open(fileName, "r")
+    return os.path.getsize(fileName)
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    # print(args)
+    if len(args) == 0:
+        print("You have not provided any option. To understand the possible options, use the flag '--help'")
+    else:
+        flag = args[0]
+        if flag == "--help":
+            print("\nThe following options are available in the WC tool:\n\n"
+                  "\n-c   -->   Counts the number of bytes of a given file"
+                  "\n-l   -->   Counts the number of lines in the given file"
+                  "\n-w   -->   Counts the number of words in the given file"
+                  "\n-m   -->   Counts the number of characters in the given file"
+
+                  "\n\nNote: You need to provide the file name after the chosen option, and make sure that the file "
+                  "is in the same directory as your pwd\n")
+        elif flag == "-c":
+            try:
+                fileName = args[1]
+                fileBytes = countBytes(fileName)
+                print(fileBytes, fileName)
+            except Exception as err:
+                print("You're required to give a file name. Either there is no name given, or the given file isn't "
+                      "present in your pwd")
+        else:
+            print("Unknown, so far")
