9b2617c7f031e29a6878c1d0e5ba0fec90be6eb1
Sarthak Khanduja <sarthakkhanduja98@gmail.com>
2024-05-30 22:13:59 -0400

    Completed the coding challenge

--- better-solution.py
+++ better-solution.py
@@ -1,31 +1,38 @@
 import os
-import sys
 import argparse
+import sys
 
 
-def countBytes(fileName):
-    return os.path.getsize(fileName)
+def countBytes(file):
+    if file == sys.stdin:
+        return len(sys.stdin.read())
+    else:
+        return len(file.read().encode('utf-8'))
 
 
-def countLines(fileName):
-    f = open(fileName, "r", encoding="utf8")
-    num_lines = sum(1 for i in f)
+def countLines(file):
+    num_lines = sum(1 for i in file)
     return num_lines
 
 
-def countWords(fileName):
-    f = open(fileName, "r", encoding="utf8")
-    data = f.read()
+def countWords(file):
+    data = file.read()
     words = data.split()
     return len(words)
 
 
-def countChar(fileName):
+def countChar(file):
     chars = 0
-    with open(fileName, 'r', encoding='utf-8') as file:
-        for line in file:
-            chars += len(line) + 1
+    for line in file:
+        chars += len(line) + 1
     return chars
+
+
+def process_file(file):
+    if file == '-':
+        return sys.stdin
+    else:
+        return open(file, "r", encoding="utf8")
 
 
 if __name__ == "__main__":
@@ -35,21 +42,38 @@ if __name__ == "__main__":
     parser.add_argument("-w", help="Calculates the number of words in the file", action="store_true")
     parser.add_argument("-m", help="Calculates the number of characters in the file", action="store_true")
 
-    parser.add_argument("fileName", help="The name of the file to process")
+    parser.add_argument("fileName", nargs='?', default='-', help="The name of the file to process")
 
     args = parser.parse_args()
 
     if args.c:
-        print(countBytes(args.fileName), args.fileName)
+        # if args.fileName == '-':
+        #     print(countBytes(process_file(args.fileName)))
+        # else:
+        #     print(countBytes(process_file(args.fileName)), args.fileName)
+        size = countBytes(process_file(args.fileName))
+        print(size, args.fileName)
 
     if args.l:
-        print(countLines(args.fileName), args.fileName)
+        if args.fileName == '-':
+            print(countLines(process_file(args.fileName)))
+        else:
+            print(countLines(process_file(args.fileName)), args.fileName)
 
     if args.w:
-        print(countWords(args.fileName), args.fileName)
+        if args.fileName == '-':
+            print(countWords(process_file(args.fileName)))
+        else:
+            print(countWords(process_file(args.fileName)), args.fileName)
 
     if args.m:
-        print(countChar(args.fileName), args.fileName)
+        if args.fileName == '-':
+            print(countChar(process_file(args.fileName)))
+        else:
+            print(countChar(process_file(args.fileName)), args.fileName)
 
     if not(args.m or args.w or args.l or args.c) and args.fileName:
-        print(countLines(args.fileName), countWords(args.fileName), countChar(args.fileName), args.fileName)
+        if args.fileName == '-':
+            print(countChar(process_file(args.fileName)), countLines(process_file(args.fileName)), countWords(process_file(args.fileName)))
+        else:
+            print(countChar(process_file(args.fileName)), countLines(process_file(args.fileName)), countWords(process_file(args.fileName)), args.fileName)
