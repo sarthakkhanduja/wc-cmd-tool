6f74e564472e171a45f3add6d4d0ff12b555617d
Sarthak Khanduja <sarthakkhanduja98@gmail.com>
2024-05-30 22:31:20 -0400

    Made the file executable through terminal

--- better-solution.py
+++ better-solution.py
@@ -1,26 +1,30 @@
-import os
 import argparse
 import sys
 
 
+# Function to count the number of bytes for a given file
 def countBytes(file):
+    # In case we need to read the file from standard input
     if file == sys.stdin:
         return len(sys.stdin.read())
     else:
         return len(file.read().encode('utf-8'))
 
 
+# Function to count the number of lines in a file
 def countLines(file):
     num_lines = sum(1 for i in file)
     return num_lines
 
 
+# Function to count the number of words in a file
 def countWords(file):
     data = file.read()
     words = data.split()
     return len(words)
 
 
+# Function to count the number of characters in the file
 def countChar(file):
     chars = 0
     for line in file:
@@ -28,6 +32,7 @@ def countChar(file):
     return chars
 
 
+# Function to handle the file processing depending on whether it's a given file or a standard input
 def process_file(file):
     if file == '-':
         return sys.stdin
@@ -36,21 +41,24 @@ def process_file(file):
 
 
 if __name__ == "__main__":
+    # Initializing the parser
     parser = argparse.ArgumentParser(description="A simple word count tool using Python")
+
+    # Adding different arguments
+    # Note - Using "action" makes it a boolean determining the presence of the argument
     parser.add_argument("-c", help="Calculates the size of the file, in bytes", action="store_true")
     parser.add_argument("-l", help="Calculates the number of lines in the file", action="store_true")
     parser.add_argument("-w", help="Calculates the number of words in the file", action="store_true")
     parser.add_argument("-m", help="Calculates the number of characters in the file", action="store_true")
 
+    # Adding the final argument of file name, and making it optional to allow reading from standard input
     parser.add_argument("fileName", nargs='?', default='-', help="The name of the file to process")
 
+    # Parsing the arguments
     args = parser.parse_args()
 
+    # Case wise handling of situations for different arguments
     if args.c:
-        # if args.fileName == '-':
-        #     print(countBytes(process_file(args.fileName)))
-        # else:
-        #     print(countBytes(process_file(args.fileName)), args.fileName)
         size = countBytes(process_file(args.fileName))
         print(size, args.fileName)
 
